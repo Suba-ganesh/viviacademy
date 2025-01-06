361efb8ffb3f45bf95248328aeaa941008e9fdf9
Ranjith-41 <ranjithkittusamy.com>
2025-01-06 23:27:22 +0530

    created course component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home';
 import CustomNavbar from './Components/Navbar/Navbar';
+import CourseCards from './Components/Course/Course';
+import CourseDetails from './Components/CourseDetails/CourseDetails';
+import CollegeCourse from './Components/College/College';
+import ProfessionalCourse from './Components/Professional/Professional';
 
 function App() {
   return (
     <div className="App">
-      <CustomNavbar/>
-      <Home />
-      <Footer/>
+      <Router>
+        <CustomNavbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={
+              <div>
+                <Home />
+                <CourseCards />
+                <CollegeCourse />
+                <ProfessionalCourse />
+              </div>
+            }
+            />
+            <Route path="/school-courses" element={<CourseCards/>} />
+            <Route path="/college-courses" element={<CollegeCourse/>} />
+            <Route path="/professional-courses" element={<ProfessionalCourse/>} />
+
+            <Route path="/course/:dataset/:id" element={<CourseDetails />} />
+          </Routes>
+        </div>
+        <Footer />
+
+      </Router>
+
     </div>
   );
 }
